4d4af2b3f6a4f53bef70ab97d68f4dc85fcb56e9
OmerGuven2i <omer.guven@2itesting.com>
2023-08-17 09:57:40 +0100

    simpleTest for jenkins

---
+++ src/test/java/com/JiraJenkins/simpleTest.java
@@ -0,0 +1,44 @@
+package com.JiraJenkins;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class simpleTest {
+
+    @Test
+    public void basicTest1(){
+        int mynumber = 5;
+        assertEquals(mynumber, 2);
+    }
+
+    @Test
+    public void basicTest2(){
+        int mynumber = 5;
+        assertEquals(mynumber, 3);
+    }
+
+    @Test
+    public void basicTest3(){
+        int mynumber = 5;
+        assertEquals(mynumber, 4);
+    }
+
+
+    @Test
+    public void basicTest4(){
+        int mynumber = 5;
+        assertEquals(mynumber, 1);
+    }
+
+    @Test
+    public void basicTest5(){
+        int mynumber = 5;
+        assertEquals(mynumber, 5);
+    }
+
+
+}
